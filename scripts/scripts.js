a34b984643eb7bee4ee4ff158aec202053245bfe
dpmott <dpmott@gmail.com>
2018-02-24 09:38:45 -0500

    Adding javascript to briefly hide body content on load.

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+function hideThenShow() {
+  var myDiv = document.getElementById('content');
+  console.log('Hiding content', myDiv);
+  myDiv.hidden = true;
+  myDiv.style.display = 'none';
+  setInterval(function(){
+    console.log('Showing content', myDiv);
+    myDiv.hidden = false;
+    myDiv.style.display = 'block';
+  }, 500);
+}
